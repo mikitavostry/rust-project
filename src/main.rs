25573bd86ef57e5737bf527a3d87d575e910dfd6
Mikita <vostrymikita@gmail.com>
2022-08-28 18:40:23 +0200

    changed mounting

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,25 @@
-use rand::Rng;
-
 #[macro_use]
 extern crate rocket;
 
+use rand::Rng;
+
 fn randomFromRange(from: i64, to: i64) -> i64 {
     let mut rng = rand::thread_rng();
-    let randInt: i64 = rng.gen_range(from..to);
-    return randInt;
+    return rng.gen_range(from..to);
 }
 
 #[get("/calculateDisselUsageForDistance?<distance>&<yearOfProduction>&<fuelUsagePer100KM>")]
 fn calculateDiselUsage(distance: u32, yearOfProduction: u32, fuelUsagePer100KM: u32) -> String {
     let fuelUsage: f32 = (distance * fuelUsagePer100KM) as f32 / 100.0;
-    format!("Fuel consumption is {}.", fuelUsage)
+    format!("{}", fuelUsage)
 }
 
 #[get("/probabilityOfUnitInjectorFail?<VIN>")]
 fn findProbability(VIN: String) -> String {
-    format!(
-        "The chance that the engine will fail is {}%.",
-        randomFromRange(0, 100)
-    )
+    format!("{}%", randomFromRange(0, 100))
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount("/api", routes![calculateDiselUsage])
-        .mount("/api", routes![findProbability])
+    rocket::build().mount("/api", routes![calculateDiselUsage, findProbability])
 }
